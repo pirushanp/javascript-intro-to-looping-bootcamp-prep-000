45330dd348935486c216b0451476ad6ee6138547
pirushanp <pirushanp@gmail.com>
2017-10-27 18:22:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 for (let i = 0; i < 25; i++) {
   if i == 1 ;
-  return "I am 1 strange loop."
+  var array = "I am 1 strange loop."
+  return array
   else ;
   return "I am ${i} strange loops."
 }
